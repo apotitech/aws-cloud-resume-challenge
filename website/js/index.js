1af8e16fca990da3c0f3f14d2dce534d01b29a47
apotitech <lioneltchami@gmail.com>
2023-09-04 18:33:22 -0600

    Updated counter function

--- website/js/index.js
+++ website/js/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://ebt2idsxwlt4lwy3y34g7far440wyhwc.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://2vjjrmcamqee4245hj5kku3psm0ctkvc.lambda-url.us-east-1.on.aws//");
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
 }
